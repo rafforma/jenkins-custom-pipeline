98483ad6166e4b75c4719c8471785a9c8f69f616
rafforma <raffaele.formato@gmail.com>
2019-09-17 15:09:02 +0200

    Update mypipeline.groovy

--- vars/mypipeline.groovy
+++ vars/mypipeline.groovy
@@ -16,47 +16,45 @@ def call(Map pipelineParams) {
             stage('build') {
                 steps {
                     //sh 'mvn package -Dmaven.test.skip=true -DgitlabBranch=${MygitlabBranch} -Drevision=${revision}'
-                    sh 'echo "mvn clean package -DskipTests=true"'
+                
                 }
             }
 
             stage ('test') {
-		when {not env.fastdeploy}
+		
                 steps {
                     //sh 'mvn test -Dmaven.test.failure.ignore=false -DgitlabBranch=${MygitlabBranch} -Drevision=${revision}'
                 }
             }
 
             stage('sonar'){
-		when {not env.fastdeploy}
+		
                 steps {
-                    //deploy(pipelineParams.developmentServer, pipelineParams.serverPort)
-                    sh 'mvn sonar:sonar -Dmaven.test.failure.ignore=true -DgitlabBranch=${MygitlabBranch} -Drevision=${revision} -Dsonar.projectName=${My_Job} -Dsonar.projectKey=${My_Job} -Dsonar.projectVersion=${revision}'
+                
+                   // sh 'mvn sonar:sonar -Dmaven.test.failure.ignore=true -DgitlabBranch=${MygitlabBranch} -Drevision=${revision} -Dsonar.projectName=${My_Job} -Dsonar.projectKey=${My_Job} -Dsonar.projectVersion=${revision}'
                 }
             }
 		
 	    stage('fortify'){
-	         when {not env.fastdeploy}
+	        
 	        steps {	    
-	    	   sh """
+	    	   /* sh """
 			if [[ "$MygitlabActionType" = "TAG_PUSH" ]] 
 			then
 			sudo ansible-playbook /etc/ansible/playbooks/openshift_deploy/fortify_report.yml --extra-vars "workspace=${Workspace_Home} project=${My_Job} tag=${revision} gitlabgroup=${git_prj_path}"
 			fi		   
-		   """
+		   """*/
 	 	}
 	   }
 
             stage('deploy staging'){
-                steps {
-                    //deploy(pipelineParams.stagingServer, pipelineParams.serverPort)
+                steps {                
                     sh 'echo deploy staging'
                 }
             }
 
             stage('deploy production'){
-                steps {
-                    //deploy(pipelineParams.productionServer, pipelineParams.serverPort)
+                steps {                   
                     sh 'echo deploy production'
                 }
             }
