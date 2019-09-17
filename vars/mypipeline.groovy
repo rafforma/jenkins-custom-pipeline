f68a86be27f9741448f69b789a5814bfe6eb23f9
rafforma <raffaele.formato@gmail.com>
2019-09-17 14:53:19 +0200

    Update mypipeline.groovy

--- vars/mypipeline.groovy
+++ vars/mypipeline.groovy
@@ -5,72 +5,47 @@ def call(Map pipelineParams) {
         stages {
             stage('checkout git') {
                 steps {
-                    // git branch: pipelineParams.branch, credentialsId: 'GitCredentials', url: pipelineParams.scmUrl
-                    sh """
-                    
-                    echo My_Job=${JOB_NAME} > ${HOME}/jobs/${JOB_NAME}/${JOB_NAME}.properties
-                        echo Workspace_Home=${HOME}/workspace/${JOB_NAME} >> ${HOME}/jobs/${JOB_NAME}/${JOB_NAME}.properties
-                        echo MygitlabBranch=${gitlabBranch} >> ${HOME}/jobs/${JOB_NAME}/${JOB_NAME}.properties
-                        echo revision=${gitlabBranch#refs/tags/} >> ${HOME}/jobs/${JOB_NAME}/${JOB_NAME}.properties
-                        echo MygitlabActionType=${gitlabActionType} >> ${HOME}/jobs/${JOB_NAME}/${JOB_NAME}.properties
-                        if grep -q "\/*\.fast$" ${HOME}/jobs/${JOB_NAME}/${JOB_NAME}.properties; then
-	                        echo fastdeploy=true >> ${HOME}/jobs/${JOB_NAME}/${JOB_NAME}.properties
-                        else
-	                        echo fastdeploy=false >> ${HOME}/jobs/${JOB_NAME}/${JOB_NAME}.properties
-                        fi
-                        if grep -q "\/*\.release$" ${HOME}/jobs/${JOB_NAME}/${JOB_NAME}.properties; then
-	                        echo release=true >> ${HOME}/jobs/${JOB_NAME}/${JOB_NAME}.properties
-                        else
-	                        echo release=false >> ${HOME}/jobs/${JOB_NAME}/${JOB_NAME}.properties
-                        fi
-
-                        echo docker_img=false >> ${HOME}/jobs/${JOB_NAME}/${JOB_NAME}.properties
-                        echo git_prj_path=DEV_ACN >> ${HOME}/jobs/${JOB_NAME}/${JOB_NAME}.properties
-                        echo fortify=false >> ${HOME}/jobs/${JOB_NAME}/${JOB_NAME}.properties
-                        echo ng_options= >> ${HOME}/jobs/${JOB_NAME}/${JOB_NAME}.properties
-                        echo appURL=webapp-bu >> ${HOME}/jobs/${JOB_NAME}/${JOB_NAME}.properties
-
-                        echo gitlabURL=${gitlabSourceRepoHttpUrl} >> ${HOME}/jobs/${JOB_NAME}/${JOB_NAME}.properties
-
-                        if [[ $gitlabBranch = *".dev"* ]]; then
-                            echo Base_Dir=/appl_bin/apache/2.4.33/htdocs >> ${HOME}/jobs/${JOB_NAME}/${JOB_NAME}.properties
-                            echo PaaS=pipeline-dev >> ${HOME}/jobs/${JOB_NAME}/${JOB_NAME}.properties
-                            echo host=hosts_dev >> ${HOME}/jobs/${JOB_NAME}/${JOB_NAME}.properties
-                        elif [[ $gitlabBranch = *".evo"* ]]; then
-                            echo Base_Dir=/appl_bin/apache/2.4.33/htdocs/evo >> ${HOME}/jobs/${JOB_NAME}/${JOB_NAME}.properties
-                            echo PaaS=pipeline-evo >> ${HOME}/jobs/${JOB_NAME}/${JOB_NAME}.properties
-                            echo host=hosts_evo >> ${HOME}/jobs/${JOB_NAME}/${JOB_NAME}.properties
-                        elif [[ "${gitlabBranch}" =~ "pre" ]]; then
-                            echo Base_Dir=null >> ${HOME}/jobs/${JOB_NAME}/${JOB_NAME}.properties
-                            echo PaaS=null >> ${HOME}/jobs/${JOB_NAME}/${JOB_NAME}.properties
-                        else
-                            echo Base_Dir=null >> ${HOME}/jobs/${JOB_NAME}/${JOB_NAME}.properties
-                            echo PaaS=null >> ${HOME}/jobs/${JOB_NAME}/${JOB_NAME}.properties
-                        fi         
-                        
-                        """
+                    // git branch: pipelineParams.branch, credentialsId: 'GitCredentials', url: pipelineParams.scmUrl                   
                 }
             }
-
+	   stage('environment setup') {
+                steps {
+                    // git branch: pipelineParams.branch, credentialsId: 'GitCredentials', url: pipelineParams.scmUrl                   
+                }
+            }
             stage('build') {
                 steps {
-                    //sh 'mvn clean package -DskipTests=true'
+                    //sh 'mvn package -Dmaven.test.skip=true -DgitlabBranch=${MygitlabBranch} -Drevision=${revision}'
                     sh 'echo "mvn clean package -DskipTests=true"'
                 }
             }
 
             stage ('test') {
+		when {not env.fastdeploy}
                 steps {
-                    sh 'echo parallel test'
+                    //sh 'mvn test -Dmaven.test.failure.ignore=false -DgitlabBranch=${MygitlabBranch} -Drevision=${revision}'
                 }
             }
 
-            stage('deploy developmentServer'){
+            stage('sonar'){
+		when {not env.fastdeploy}
                 steps {
                     //deploy(pipelineParams.developmentServer, pipelineParams.serverPort)
-                    sh 'echo deploy developmentServer'
+                    sh 'mvn sonar:sonar -Dmaven.test.failure.ignore=true -DgitlabBranch=${MygitlabBranch} -Drevision=${revision} -Dsonar.projectName=${My_Job} -Dsonar.projectKey=${My_Job} -Dsonar.projectVersion=${revision}'
                 }
             }
+		
+	    stage('fortify'){
+	         when {not env.fastdeploy}
+	        steps {	    
+	    	   sh """
+			if [[ "$MygitlabActionType" = "TAG_PUSH" ]] 
+			then
+			sudo ansible-playbook /etc/ansible/playbooks/openshift_deploy/fortify_report.yml --extra-vars "workspace=${Workspace_Home} project=${My_Job} tag=${revision} gitlabgroup=${git_prj_path}"
+			fi		   
+		   """
+	 	}
+	   }
 
             stage('deploy staging'){
                 steps {
